f027db1f24486105b2edf9d95795c6aeb087ddb2
mmjee <root@git.maharshi.ninja>
2025-03-18 13:25:04 +0530

    Deployment scripts

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
+pragma solidity ^0.8.28;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -208,7 +208,7 @@ contract Vault is Initializable, UUPSUpgradeable, AccessControlUpgradeable, Reen
     function extractAddress(Party[] memory parties) internal pure returns (address from) {
           for(uint i = 0; i < parties.length; i++) {
             if (parties[i].universe == Universe.ETHEREUM) {
-                 from = bytes32ToAddress(parties[i].address_); 
+                 from = bytes32ToAddress(parties[i].address_);
                  break;
             }
         }
